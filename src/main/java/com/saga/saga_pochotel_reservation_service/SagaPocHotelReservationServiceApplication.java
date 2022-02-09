d4edd0e8521fc5395573f3ed114f6dedcb03d103
Aaron Burk <aburk@veracode.com>
2022-02-09 11:53:14 -0500

    add spring boot app

---
+++ src/main/java/com/saga/saga_pochotel_reservation_service/SagaPocHotelReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saga.saga_pochotel_reservation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaPocHotelReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaPocHotelReservationServiceApplication.class, args);
+	}
+
+}
